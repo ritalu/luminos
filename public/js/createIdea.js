073a427009a38ff2fcdd65409c9c89d71e507ba7
Ivelin Bratanov <ivelin.bratanov@mail.mcgill.ca>
2014-11-02 05:37:54 -0500

    Remove event blocker to see if we're sent to main page after creating an idea.

--- public/js/createIdea.js
+++ public/js/createIdea.js
@@ -1,14 +1,15 @@
 var myKeyVals = { A1984 : 1, A9873 : 5, A1674 : 2, A8724 : 1, A3574 : 3, A1165 : 5 }
-var formData = {name:"ravi",age:"31"}; //Array
+var formData = {name:"Pokemon Creator",description:"3D printer which can print all Pokemon."}; //Array
 
 var createIdea=function() {
     console.log("create idea");
   $.ajax({//initial ajax call 
     type:"POST",
     url:"./api/ideas",
-    data:"submit=1&name="+idea_name,
+    data:formData,
     success: function(data){
         console.log(data);
+	console.log("submit=1&name="+idea_name);
         displayResult(data);
     }
   });
