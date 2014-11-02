3f8c8f0e031f4dc2dd22fffce7c5b26184a3db8b
reetah <rita.lu410@gmail.com>
2014-11-01 22:20:33 -0400

    js

--- public/js/getIdea.js
+++ public/js/getIdea.js
@@ -23,7 +23,35 @@ var loadProject = function() {
 }
 
 var renderProject = function (data) {
-  console.log(data);
+console.log("rendering");
+  var content="";
+  for (var i = 0; i < data.length; i++) {
+    if (data[i] == null) {
+      break;
+    }
+    else {
+      content +=
+       '<div class="idea-container">' +
+        '<div class="title">'+
+          data[i].name +
+           ' <span class="user">'+
+            ' <img src="http://exmoorpet.com/wp-content/uploads/2012/08/cat.png">' +
+            data[i].username + '</span>' +
+         '</div>' +
+         '<div class="desc">' +
+            '<div class="text">' + data[i].description + '</div>' +
+            '<a href="/"><div class="readmore">Read more</div></a>' +
+            '<div class="actions">' +
+                '<img src="../public/img/icon_like.png">' + data[i].likes.length +
+                '<img src="../public/img/icon_project.png">' + data[i].ideas.length +
+                '<img src="../public/img/icon_comment.png">' + 'data4' + '</div>' +
+         '</div>' +
+        '</div>'
+      };
+
+    }
+
+      $('.idea-container').replaceWith(content);
 }
 
 var renderIdea=function(data){
